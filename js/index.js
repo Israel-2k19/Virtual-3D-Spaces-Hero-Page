a50864d11b3d5b02f3eb6475410dbe60f1ce3503
Israel Onyemaenu <58822219+Israel-2k19@users.noreply.github.com>
2022-03-30 00:14:02 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// hero message styling
+
+const txtFull = "Hey Chief, view awesome experiences with our Virtual Reality App"; //the text goes here
+const txtArr = txtFull.split("");
+
+function getRandomTime() {
+  return Math.random() * 0.5;
+}
+function getEle(id) {
+  return document.getElementById("h1");
+}
+function ModifyTxt(el, txt) {
+  return (el.innerHTML = txt);
+}
+function writeOut(txtArr, curChar = 0) {
+  const randomTime = getRandomTime();
+  setTimeout(
+    () => {
+      const el = getEle("h1");
+      let elTxt = el.innerHTML;
+      elTxt += txtArr[curChar];
+      ModifyTxt(el, elTxt);
+      //check if next iteration is out of range
+      if (curChar + 1 == txtArr.length) {
+        return deleteOut(txtArr, txtArr.length);
+      }
+      return writeOut(txtArr, curChar + 1);
+    },
+
+    randomTime * 500
+  );
+}
+function deleteOut(txtArr, curChar = 0) {
+  const randomTime = getRandomTime();
+  setTimeout(
+    () => {
+      const el = getEle("h1");
+      let elTxt = el.innerHTML;
+      //remove
+      const elTxtArr = elTxt.split("");
+      const elTxtArrLen = elTxtArr.length;
+      console.log(elTxtArr.length - 1);
+      const newElTxt = elTxtArr.splice(0, curChar - 1).join("");
+      ModifyTxt(el, newElTxt);
+      if (elTxtArrLen == 0) {
+        return writeOut(txtArr, 0);
+      }
+      return deleteOut(txtArr, elTxtArrLen - 1);
+    },
+
+    randomTime * 200
+  );
+  return;
+}
+writeOut(txtArr);
